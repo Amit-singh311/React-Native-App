51ab39c852139db760617e2649d19033829293c4
Amit Singh <ammysingh786@gmail.com>
2018-12-30 00:38:48 +0530

    set up the environment and made some inital changes in  index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+//Import the required library to help create the component.
+import React from 'react';
+import ReactNative from 'react-native';
+
+
+//create the component
+
+
+
+//render the component
