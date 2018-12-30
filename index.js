620dac090dc59bcbfea1dfc43fe77ab91f655913
Amit Singh <ammysingh786@gmail.com>
2018-12-30 17:05:33 +0530

    added eslint configuration for rally coding, Also displayed my first text on the mobile screen

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 //Import the required library to help create the component.
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
 
 
 //create the component
-
-
+const App = () => (
+		<Text>Some text</Text>
+		);
 
 //render the component
+AppRegistry.registerComponent('Awesome', () => App);
