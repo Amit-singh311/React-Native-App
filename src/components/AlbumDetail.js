4dbdba2ec116ddd09b9ae95cdadc46b48ceba13b
Amit Singh <ammysingh786@gmail.com>
2019-01-05 15:21:30 +0530

    added the album list

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,13 @@
+//importing the library
+import React from 'react';
+import { Text, View } from 'react-native';
+
+//creating the component
+const AlbumDetail = () => {
+	return (
+		<Text> Album Detail </Text>
+	);
+};
+
+//exporting the Album Details
+export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,17 +1,23 @@
 //we generally need two imports
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import Fetch from 'react-native-fetch';
+import axios from 'axios';
 
 //creating component
 class AlbumList extends Component {
+	state = { albums: [] };
 	componentWillMount() {
-		Fetch('https://mywebsite.com/mydata.json');
+		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+		.then(response => this.setState({ albums: response.data }));
+	}
+	returnAlbums() {
+		return this.state.albums.map(album => <Text key={album.title}>{album.title}</Text>);
 	}
 	render() {
+		console.log(this.state);
 		return (
 			<View>
-				<Text>Album List!!!</Text>
+				<Text>{this.returnAlbums}</Text>
 			</View>
 			);	
    }
